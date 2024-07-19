c0503fe1e9cca452e8ebbcdec0e2457aa5db4d19
maia-ab <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-07-19 20:44:51 -0300

    agregamos nuestro equipo

--- src/app/nosotros/page.jsx
+++ src/app/nosotros/page.jsx
@@ -1,12 +1,18 @@
-
 import FTTF from "../components/tfft";
+import NuestroEquipo from "./nuestroEquipo";
 
 function ConocenosPage() {
-  return(
-  <main>
-    <FTTF TF='true' imagePath = "nosotros-cafe.png" titulo = "Nuestra Historia" texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra. Duis a arcu convallis, gravida purus eget, mollis diam."/>
+  return (
+    <main>
+      <FTTF
+        TF="true"
+        imagePath="nosotros-cafe.png"
+        titulo="Nuestra Historia"
+        texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra. Duis a arcu convallis, gravida purus eget, mollis diam."
+      />
+      <NuestroEquipo />
     </main>
-    );
+  );
 }
 
 export default ConocenosPage;
