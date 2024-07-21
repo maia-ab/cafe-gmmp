d74f82f049d56269326010a1a21d49f20c76379c
Giovanni-Martini <giovanni.martini@estudiantes.unahur.edu.ar>
2024-07-21 17:42:51 -0300

    agregada sección contacto y video

---
+++ src/app/components/navbar/rutas.jsx
@@ -0,0 +1,22 @@
+const rutas = [
+    {
+      href: "/nosotros",
+      label: "Nosotros",
+    },
+    {
+      href: "/recetas",
+      label: "Recetas",
+    },
+    ,
+    {
+      href: "/#menu",
+      label: "Menu",
+    },
+    ,
+    {
+      href: "/contacto",
+      label: "Contacto",
+    },
+  ];
+  
+export default rutas;
