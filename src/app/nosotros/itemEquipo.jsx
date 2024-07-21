7925b7f923c06b34a575bbc5879e95b544fc82d3
maia-ab <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-07-21 04:10:34 -0300

    arregle bug

--- src/app/nosotros/itemEquipo.jsx
+++ src/app/nosotros/itemEquipo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ItemEquipo(props) {
   return (
-    <div className="flex flex-col items-center ml-20">
+    <div className="flex flex-col items-center">
       <div className="w-[250px] h-[220px] m-5  rounded-[10px_10px_120px_120px] flex items-center justify-center overflow-hidden ">
         <img className="object-cover" src={props.imagen} alt="Miembro del equipo" />
       </div>
