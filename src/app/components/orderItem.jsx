099762da4b23a198ca9579c36e2a6231e7958c7f
Pri-PJ <priscilasolange.jofregil@estudiantes.unahur.edu.ar>
2024-07-21 19:59:54 -0300

    Se arreglaron las imagenes de OrderItem

--- src/app/components/orderItem.jsx
+++ src/app/components/orderItem.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function OrderItem({ item }) {
   return (
     <div className="flex items-center my-5">
-      <div className="bg-beige h-[100px] w-[100px] rounded-3xl"></div>
+      <div className="bg-beige h-[100px] w-[100px] rounded-3xl">
+      <img className="object-cover w-full h-full rounded-3xl" src={item.imagenUrl} alt={item.titulo} />
+      </div>
       <div className="mx-5">
         <p className="text-4xl">{item.titulo}<span className="text-base"> x{item.cantidad}</span></p>
       </div>
