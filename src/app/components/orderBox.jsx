b5d3cb4adf68b9d959e75fe2a625174d63ab3bd3
maia-ab <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-07-21 02:50:14 -0300

    se calcula correctamente el total

--- src/app/components/orderBox.jsx
+++ src/app/components/orderBox.jsx
@@ -5,6 +5,7 @@ import { MenuContext } from "../context/MenuContext";
 function OrderBox() {
   const {menu} = useContext(MenuContext);
   const orden = menu.filter((item) => item.cantidad > 0);
+  const total = orden.reduce((acc, item) => acc + item.precio * item.cantidad, 0)                       ;
   return (
     <div className="w-[500px] bg-dark-brown rounded-tl-3xl rounded-tr-md rounded-br-3xl rounded-bl-md px-8 py-24 text-beige flex flex-col items-center gap-8">
       <h2 className="text-5xl">Tu Orden</h2>
@@ -13,7 +14,7 @@ function OrderBox() {
           <OrderItem key={item.id} item={item} />
         ))}
       </div>
-      <p className="text-3xl">Total:</p>
+      <p className="text-3xl">Total: {total}</p>
       <button className="bg-coconut h-[50px] w-[300px] rounded-2xl text-2xl">
         Ordenar
       </button>
--- src/app/components/orderItem.jsx
+++ src/app/components/orderItem.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function OrderItem({ item }) {
   return (
-    <div className="flex items-center ">
+    <div className="flex items-center my-5">
       <div className="bg-beige h-[100px] w-[100px] rounded-3xl"></div>
       <div className="mx-5">
-        <p className="text-4xl">{item.titulo}x{item.cantidad}</p>
+        <p className="text-4xl">{item.titulo}<span className="text-base"> x{item.cantidad}</span></p>
       </div>
       <p className="ml-auto text-2xl">${item.precio}</p>
     </div>
