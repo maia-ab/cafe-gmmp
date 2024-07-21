4fa1f9ec9016759dd76b35e24c9bcf40898c8dd0
Maximiliano Almada <129397733+maximilianoalmada@users.noreply.github.com>
2024-07-21 20:16:02 -0300

    cambio nombre de

--- src/app/components/tfft.jsx
+++ src/app/components/tfft.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function FTTF({ TF = false, imagePath, titulo, texto }) {
+function imageTextComponent({ textFirst = false, imagePath, titulo, texto, imageLink = null }) {
   return (
     <div className="flex items-center justify-center">
       <div className="flex p-8 max-w-[1200px]">
-        {TF ? (
+        {textFirst ? (
           <div className="flex flex-col items-center lg:flex-row  w-full ">
             <div className="flex-1 lg:mr-8">
               <h1 className="text-4xl font-bold text-dark-brown mb-4">
@@ -13,13 +13,13 @@ function FTTF({ TF = false, imagePath, titulo, texto }) {
               <p className="text-dark-brown text-lg w-96">{texto}</p>
             </div>
             <div className="flex-shrink-0 lg:ml-8 lg:border-l lg:border-dark-brown lg:pl-8">
-              <img src={imagePath} alt={titulo} className="w-96 h-96 lg:ml-9" />
+            <a href={imageLink} target="_blank"><img src={imagePath} alt={titulo} className="w-96 h-96 lg:ml-9" /></a>
             </div>
           </div>
         ) : (
           <div className="flex flex-col lg:flex-row items-center w-full">
             <div className="flex-shrink-0 lg:mr-8 lg:border-r lg:border-dark-brown lg:pr-8">
-              <img src={imagePath} alt={titulo} className="w-96 h-96 lg:mr-8" />
+              <a href={imageLink} target="_blank"><img src={imagePath} alt={titulo} className="w-96 h-96 lg:mr-8" /></a>
             </div>
             <div className="flex-1 lg:ml-8">
               <h1 className="text-4xl font-bold text-dark-brown mb-4">
@@ -34,4 +34,4 @@ function FTTF({ TF = false, imagePath, titulo, texto }) {
   );
 }
 
-export default FTTF;
+export default imageTextComponent;
