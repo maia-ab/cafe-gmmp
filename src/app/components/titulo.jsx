c3d2529fad6e88ae50442b6b3c5083463412403f
Giovanni-Martini <giovanni.martini@estudiantes.unahur.edu.ar>
2024-07-21 19:39:43 -0300

    Ahora el video es responsive y centré el téxto de los títulos.

--- src/app/components/titulo.jsx
+++ src/app/components/titulo.jsx
@@ -5,7 +5,7 @@ function Titulo({ texto, estilo, color }) {
     <>
       <div className="flex justify-center items-center my-14">
         <hr className={`${estilo} hidden md:block`} />
-        <h3 className={`text-${color} text-6xl mx-12`}>{texto}</h3>
+        <h3 className={`text-${color} text-6xl mx-12 text-center`}>{texto}</h3>
         <hr className={`${estilo} hidden md:block`} />
       </div>
     </>
