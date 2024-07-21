9a8b37251073fd58ca9b4f6fc765fcc7d4f96eaa
Pri-PJ <priscilasolange.jofregil@estudiantes.unahur.edu.ar>
2024-07-21 19:02:50 -0300

    Se agregaron las imagenes al menu

--- src/app/data/data.jsx
+++ src/app/data/data.jsx
@@ -1,42 +1,42 @@
 const menuLista = [
     {
         titulo: 'Latte',
-        imagenUrl: 'url',
+        imagenUrl: 'https://images.unsplash.com/photo-1561882468-9110e03e0f78?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGxhdHRlfGVufDB8fDB8fHww',
         precio: 6000,
         cantidad: 0,
         id: 1
     },
     {
-        titulo: 'Iced Latte',
-        imagenUrl: 'url',
+        titulo: 'Americano',
+        imagenUrl: 'https://images.unsplash.com/photo-1532004491497-ba35c367d634?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         precio: 5000,
         cantidad: 0,
         id: 2
     },
     {
         titulo: 'Macchiatto',
-        imagenUrl: 'url',
+        imagenUrl: 'https://images.unsplash.com/photo-1557772611-722dabe20327?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         precio: 5000,
         cantidad: 0,
         id: 3
     },
     {
         titulo: 'Matcha Latte',
-        imagenUrl: 'url',
+        imagenUrl: 'https://images.unsplash.com/photo-1536256263959-770b48d82b0a?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWF0Y2hhJTIwbGF0dGV8ZW58MHx8MHx8fDA%3D',
         precio: 5000,
         cantidad: 0,
         id: 4
     },
     {
         titulo: 'Capuccino',
-        imagenUrl: 'url',
+        imagenUrl: 'https://images.unsplash.com/photo-1557006021-b85faa2bc5e2?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         precio: 5000,
         cantidad: 0,
         id: 5
     },
     {
-        titulo: 'Frapuccino',
-        imagenUrl: 'url',
+        titulo: 'Islatte Mocha',
+        imagenUrl: 'https://images.unsplash.com/photo-1642647391072-6a2416f048e5?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fGljZWQlMjBtb2NoYSUyMGxhdHRlfGVufDB8fDB8fHww',
         precio: 5000,
         cantidad: 0,
         id: 6
